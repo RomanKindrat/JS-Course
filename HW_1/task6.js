d7541523024088ccc6cc96bebb0960b960f4a5c1
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-06-16 14:30:24 +0300

    task6_done

---
+++ HW_1/task6.js
@@ -0,0 +1,12 @@
+const _name = "Roman";
+const surname = "Kindrat";
+const study_group = "JS Fundamentals";
+const year_of_birth = 2003;
+const married = false;
+
+console.log(`${year_of_birth}, ${married}, ${_name}, ${surname}, ${study_group}.`);
+
+let work = null;
+let salary;
+
+console.log(typeof work, typeof salary);
