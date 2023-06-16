ba08a256b78900608a913b8dbcc72a08c9999ca5
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-06-16 14:44:35 +0300

    task7_done

---
+++ HW_1/task7.js
@@ -0,0 +1,5 @@
+const login = prompt("Login");
+const email = prompt("Email");
+const password = prompt("Password");
+
+alert(`Dear ${login}, your email is ${email}, your password is ${password}.`);
