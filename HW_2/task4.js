f4b2350921938d92193c8a40f34319de9166d88b
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-06-17 19:47:49 +0300

    task 4 done

---
+++ HW_2/task4.js
@@ -0,0 +1,18 @@
+const a = Number(prompt("a = ..."));
+const b = Number(prompt("b = ..."));
+const c = Number(prompt("c = ..."));
+if(isNaN(a) || isNaN(b) || isNaN(c))
+    alert("Incorrect data");
+else
+{
+    p = (a + b + c) / 2;
+    const S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    const check = c == (Math.sqrt(Math.pow(a,2) + Math.pow(b,2))) || b == (Math.sqrt(Math.pow(a,2) + Math.pow(c,2))) || a == (Math.sqrt(Math.pow(b,2) + Math.pow(c,2)));
+    if(check)
+        console.log("The triangle is right-angled");
+    else
+        console.log("The triangle is not right-angled");
+    console.log("S = ", S.toPrecision(4));
+}    
+
+
