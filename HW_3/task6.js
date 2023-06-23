3fb9eb297a6086b44a4e56bf9f1a020dddd121b1
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-06-23 23:48:56 +0300

    task6 done

---
+++ HW_3/task6.js
@@ -0,0 +1,15 @@
+function calc(a, b, op)
+{
+    switch (op) {
+        case 1:
+            return a - b;
+        case 2:
+            return a * b;
+        case 3:
+            return a / b;
+        default:
+            return a + b;
+    }
+}
+
+console.log(calc(10, 3, 1));
