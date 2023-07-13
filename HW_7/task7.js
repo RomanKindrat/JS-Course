b61e2638ea2b6f75dd0b8025796fc8ab265acae0
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-07-13 19:57:37 +0300

    task7 done

---
+++ HW_7/task7.js
@@ -0,0 +1,29 @@
+let countrySelect = document.getElementById("country");
+let citySelect = document.getElementById("cities");
+let displayText = document.querySelector("p");
+
+let citiesByCountry = {
+    "ger": ["Berlin", "Munich", "Hamburg"],
+    "usa": ["New York", "Los Angeles", "Chicago"],
+    "ukr": ["Kyiv", "Lviv", "Odesa"]
+};
+
+function populateCities() {
+    let selectedCountry = countrySelect.value;
+    let cities = citiesByCountry[selectedCountry];
+
+    citySelect.innerHTML = "";
+
+    for (let i = 0; i < cities.length; i++) {
+        let option = document.createElement("option");
+        option.text = cities[i];
+        option.value = cities[i];
+        citySelect.appendChild(option);
+    }
+
+    displayText.innerHTML = countrySelect.options[countrySelect.selectedIndex].text + ", " + citySelect.options[citySelect.selectedIndex].text;
+}
+
+countrySelect.addEventListener("change", populateCities);
+
+populateCities();
