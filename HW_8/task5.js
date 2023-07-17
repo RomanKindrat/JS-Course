ccf118ab6a5813972f2c6a178f9738cc0beae48d
RomanKindrat <108233109+RomanKindrat@users.noreply.github.com>
2023-07-17 17:45:19 +0300

    task5 fixed

--- HW_8/task5.js
+++ HW_8/task5.js
@@ -1,6 +1,6 @@
 function checkEmail(email)
 {
-    const pattern = /^[A-Za-z0-9]+([_-][A-Za-z0-9]+)*@[A-Za-z0-9]+([_-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$/;
+    const pattern = /^[A-Za-z0-9]+(-?_*[A-Za-z0-9]+)*@[A-Za-z0-9]+(-?_*[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/;
 
     if(pattern.test(email))
         console.log("Email is correct!");
@@ -13,3 +13,5 @@ checkEmail('my_mail@gmail.com');
 checkEmail('#my_mail@gmail.com');
 
 checkEmail('my_ma--il@gmail.com');
+
+checkEmail('my__ma-il@gmail.com');
